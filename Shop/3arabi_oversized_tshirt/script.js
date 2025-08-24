f892e0e44c5fc8c711ba4c8a224fed575c10af7f
3arabystore <mohamedsafwat7706@gmail.com>
2025-08-25 02:45:50 +0300

    Add files via upload

---
+++ Shop/3arabi_oversized_tshirt/script.js
@@ -0,0 +1,20 @@
+
+const pics = ["image1", "image2", "image3"];
+
+
+function next(){
+    var image = document.getElementById("image")
+    var img = image.style.backgroundImage.replace('url(', '').replace(')', '').replace(/["']/g, "");
+
+    if(img == "image1.jpeg"){
+        image.style.backgroundImage = "url('image2.jpeg')"
+    }
+    else if(img == "image2.jpeg"){
+        image.style.backgroundImage = "url('image3.png')"
+    }
+    else if(img == "image3.png"){
+        image.style.backgroundImage = "url('image1.jpeg')"
+    }
+    
+    
+}
